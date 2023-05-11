067260d9fbd5caa9280d1d1f05fee21bd823f2c9
Alex Roan <alex.roan@hotmail.com>
2023-05-11 11:02:09 +0100

    GovernorZK compiles

--- contracts/GovernorZK.sol
+++ contracts/GovernorZK.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {IGovernor, Governor, IERC165} from "@openzeppelin/contracts/governance/Governor.sol";
-import {GovernorCompatibilityBravo} from
-    "@openzeppelin/contracts/governance/compatibility/GovernorCompatibilityBravo.sol";
+import {GovernorCompatibilityZK} from "./compatibility/GovernorCompatibilityZK.sol";
 import {IVotes, GovernorVotes} from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import {GovernorVotesQuorumFraction} from
     "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
@@ -15,16 +14,17 @@ import {ZKTree, IHasher, IVerifier} from "zk-merkle-tree/contracts/ZKTree.sol";
 
 contract GovernorZK is
     Governor,
-    GovernorCompatibilityBravo,
     GovernorVotes,
     GovernorVotesQuorumFraction,
     GovernorTimelockControl,
+    GovernorCompatibilityZK,
     ZKTree
 {
     error WrongState(ProposalState actual, ProposalState expected);
     error AlreadyCommitted(uint256 proposalId, address voter);
     error NotEligible(address voter);
     error InvalidCommitment(uint256 commitment);
+    error NotImplemented();
 
     mapping(uint256 proposalId => mapping(address voter => bool commited)) public s_hasCommitted;
 
@@ -63,8 +63,40 @@ contract GovernorZK is
         s_hasCommitted[proposalId][msg.sender] = true;
     }
 
-    // TODO Override and revert existing castVote functions
+    /// @dev this overrides _castVote. All castVote functions in inherited contracts
+    /// that call it therefore revert. We use our own caseVote function that uses ZK proofs.
+    function _castVote(uint256, address, uint8, string memory, bytes memory) internal pure override returns (uint256) {
+        revert NotImplemented();
+    }
+
     // TODO Implement new castVote functions with ZK proofs
+    function castVote(
+        uint256 proposalId,
+        uint8 support,
+        uint256 nullifier,
+        uint256 root,
+        uint256[2] calldata proof_a,
+        uint256[2][2] calldata proof_b,
+        uint256[2] calldata proof_c
+    ) external {
+        // Check that the state is active
+        ProposalState proposalState = state(proposalId);
+        if (proposalState != ProposalState.Active) revert WrongState(proposalState, ProposalState.Active);
+
+        // nullify the commitment
+        _nullify(bytes32(nullifier), bytes32(root), proof_a, proof_b, proof_c);
+
+        _countVote(
+            proposalId,
+            bytes32(nullifier),
+            support,
+            1 // hard coded for now... Perhaps have the circuit prove that the weight is correct?
+                // TODO Pass it in as a param when casting a vote? Something to think about later
+        );
+
+        // TODO event
+        // emit VoteCast(nullifier, proposalId, support, 1);
+    }
 
     /// BLOILERPLATE BELOW ///
 
@@ -96,7 +128,7 @@ contract GovernorZK is
         uint256[] memory values,
         bytes[] memory calldatas,
         string memory description
-    ) public override(Governor, GovernorCompatibilityBravo, IGovernor) returns (uint256) {
+    ) public override(Governor, IGovernor, GovernorCompatibilityZK) returns (uint256) {
         return super.propose(targets, values, calldatas, description);
     }
 
